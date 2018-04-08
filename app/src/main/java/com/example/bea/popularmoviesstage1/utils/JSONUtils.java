31b1b0a253d4d8093f2803c5be403600d8523dbb
BBtat23 <31885469+BBtat23@users.noreply.github.com>
2018-04-08 08:55:13 +0200

    Update JSONUtils.java

--- app/src/main/java/com/example/bea/popularmoviesstage1/utils/JSONUtils.java
+++ app/src/main/java/com/example/bea/popularmoviesstage1/utils/JSONUtils.java
@@ -1,13 +1,3 @@
-package com.example.bea.popularmoviesstage1.utils;
-
-import com.example.bea.popularmoviesstage1.data.Movie;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class JSONUtils {
@@ -18,6 +8,8 @@ public class JSONUtils {
         String overView = null;
         Double ratingUser = null;
         String releaseDate = null;
+        String idMovieString = null;
+
 
         JSONObject jsonObjectMovie = new JSONObject(jsonMovie);
         JSONArray jsonArrayMovie = jsonObjectMovie.getJSONArray("results");
@@ -30,10 +22,22 @@ public class JSONUtils {
             overView = movieObject.getString("overview");
             ratingUser = movieObject.getDouble("vote_average");
             releaseDate = movieObject.getString("release_date");
-
-            objectMovie = new Movie(originalTitle, posterPath, overView, ratingUser, releaseDate);
+            int idMovie = movieObject.getInt("id");
+            idMovieString = String.valueOf(idMovie);
+            objectMovie = new Movie(originalTitle, posterPath, overView, ratingUser, releaseDate,idMovieString);
             movies.add(objectMovie);
         }
         return movies;
+    }
+
+    public static String movieVideoKey (String movieVideoString) throws JSONException {
+        String videoKeyString = null;
+        JSONObject jsonObjectIdMovie = new JSONObject(movieVideoString);
+        JSONArray idMovieJsonArray = jsonObjectIdMovie.getJSONArray("results");
+        for (int i = 0; i < idMovieJsonArray.length(); i++){
+            JSONObject idMovieObject = idMovieJsonArray.getJSONObject(i);
+            videoKeyString = idMovieObject.getString("key");
+        }
+        return videoKeyString;
     }
 }
