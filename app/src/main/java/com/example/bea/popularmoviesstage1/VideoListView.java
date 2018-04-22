edd37ee7887d16ca05a1455c7f283385f2981806
BBtat23 <31885469+BBtat23@users.noreply.github.com>
2018-04-22 17:24:00 +0200

    Create VideoListView.java

---
+++ app/src/main/java/com/example/bea/popularmoviesstage1/VideoListView.java
@@ -0,0 +1,29 @@
+package com.example.bea.popularmoviesstage1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+
+public class VideoListView extends ListView {
+
+    public VideoListView  (Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VideoListView  (Context context) {
+        super(context);
+    }
+
+    public VideoListView  (Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
+}
