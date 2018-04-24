ee4d3530a0a4c60813e92da5f0705b146a988bd8
BBtat23 <31885469+BBtat23@users.noreply.github.com>
2018-04-24 15:28:25 +0200

    Update MovieDetailActivity.java

--- app/src/main/java/com/example/bea/popularmoviesstage1/MovieDetailActivity.java
+++ app/src/main/java/com/example/bea/popularmoviesstage1/MovieDetailActivity.java
@@ -134,7 +134,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         favouriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                insertMovie();
             }
         });
     }
